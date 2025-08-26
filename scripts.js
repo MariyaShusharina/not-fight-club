419dde842b4bd9c70c44c2dc981a32b042c6b453
Mariya Shusharina <mariya.shushar@gmail.com>
2025-08-26 06:18:08 +0300

    feat: add registration page, error dialog, character data object, localstorage helpers and fix event listeners on buttons

--- scripts.js
+++ scripts.js
@@ -1,11 +1,34 @@
 
-// Variables
+/* Variables */
 
 const charName = '';
 const wins = 0;
 const loses = 0;
 
-// Generation of the Page content
+/* Data Object */
+
+let data = {
+  charName: '',
+  avatar: 0,
+  wins: 0,
+  loses: 0,
+};
+
+/* Generation of the Page content */
+
+const errorDialog = document.createElement('dialog');
+errorDialog.classList.add('error-dialog');
+  const errorText = document.createElement('p');
+  errorText.textContent = 'Error occured!';
+
+  const closeErrorBtn = document.createElement('button');
+  closeErrorBtn.classList.add('btn');
+  closeErrorBtn.textContent = 'Close';
+  closeErrorBtn.addEventListener('click', closeDialog);
+errorDialog.appendChild(errorText);
+errorDialog.appendChild(closeErrorBtn);
+
+document.body.appendChild(errorDialog);
 
 const header = document.createElement('header');
   const headerDiv = document.createElement('div');
@@ -16,15 +39,15 @@ const header = document.createElement('header');
     const nav = document.createElement('nav');
       const battleBtn = document.createElement('button');
       battleBtn.classList.add('nav-btn', 'battle');
-      battleBtn.addEventListener('click', battlePage());
+      battleBtn.addEventListener('click', battlePage);
 
       const profileBtn = document.createElement('button');
       profileBtn.classList.add('nav-btn', 'profile');
-      profileBtn.addEventListener('click', profilePage());
+      profileBtn.addEventListener('click', profilePage);
 
       const editBtn = document.createElement('button');
       editBtn.classList.add('nav-btn', 'edit');
-      editBtn.addEventListener('click', edit());
+      editBtn.addEventListener('click', edit);
     nav.appendChild(battleBtn);
     nav.appendChild(profileBtn);
     nav.appendChild(editBtn);
@@ -33,6 +56,33 @@ const header = document.createElement('header');
 header.appendChild(headerDiv);
 
 const main = document.createElement('main');
+  const mainDiv = document.createElement('div');
+  mainDiv.classList.add('main-div');
+    const regPage = document.createElement('div');
+    regPage.classList.add('reg-div');
+      const regH2 = document.createElement('h2');
+      regH2.textContent = 'Create your character';
+
+      const regLabel = document.createElement('label');
+      regLabel.textContent = 'Character name:';
+      regLabel.setAttribute('for', 'charname');
+
+      const regInput = document.createElement('input');
+      regInput.setAttribute('id', 'charname');
+      regInput.setAttribute('type', 'text');
+      regInput.setAttribute('minlength', 1);
+      regInput.setAttribute('maxlength', 40);
+
+      const regBtn = document.createElement('button');
+      regBtn.classList.add('btn');
+      regBtn.textContent = 'Create!';
+      regBtn.addEventListener('click', createCharacter);
+    regPage.appendChild(regH2);
+    regPage.appendChild(regLabel);
+    regPage.appendChild(regInput);
+    regPage.appendChild(regBtn);
+  mainDiv.appendChild(regPage);
+main.appendChild(mainDiv);
 
 const footer = document.createElement('footer');
   const footerDiv = document.createElement('div');
@@ -78,11 +128,56 @@ document.body.appendChild(header);
 document.body.appendChild(main);
 document.body.appendChild(footer);
 
-/* Functionality */
+/* Onload functionality */
+
+getLS();
+if (data.charName !== '') {
+  regPage.classList.add('hid');
+}
+
+/* Navigation */
 
 function battlePage() {}
 
 function profilePage() {}
 
 function edit() {}
+
+/* Functionality */
+
+function createCharacter() {
+  const val = regInput.value.toString();
+  if (val.length > 1 && val.length < 41) {
+    data.charName = val;
+    writeLS(data);
+    regPage.classList.add('hid');
+  } else {
+    throwDialogError('Error: Invalid Character Name!');
+  }
+}
+
+/* Error Dialog functionality */
+
+function throwDialogError(message = 'Error occured!') {
+  errorDialog.showModal();
+  errorText.textContent = message;
+}
+
+function closeDialog() {
+  errorDialog.close();
+}
+
+/* Local Storage helper */
+
+function writeLS(val) {
+  localStorage.setItem('NotGenshinClub', JSON.stringify(val));
+}
+
+function getLS() {
+  if (localStorage.NotGenshinClub) {
+    data = JSON.parse(localStorage.getItem('NotGenshinClub'));
+  } else {
+    writeLS(data);
+  }
+}
 
